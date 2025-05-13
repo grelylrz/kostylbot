c8d72247c8ede00cde88fa76315f57ebdc1e1a9e
grely <grely@grely.grely>
2025-05-13 19:53:17 +0500

    fix

--- src/main/java/icu/grely/bot/commands/Spec.java
+++ src/main/java/icu/grely/bot/commands/Spec.java
@@ -1,6 +1,7 @@
 package icu.grely.bot.commands;
 
 import static icu.grely.Vars.prefix;
+import static icu.grely.bot.SendUtils.sendReply;
 import static icu.grely.bot.commands.CommandsHandler.commands;
 import static icu.grely.bot.commands.CommandsHandler.registerCommand;
 
@@ -14,18 +15,38 @@ public class Spec {
     public static void load() {
         registerCommand("help", "Посмотреть список команд.", "[command-name]", (e, args)->{
             EmbedCreateSpec.Builder em=EmbedCreateSpec.builder().title("Список команд.").color(Color.SEA_GREEN);
-            StringBuilder cname=new StringBuilder();
-            for(CommandsHandler.BotCommand c : commands) {
-                if(c.isVisible() && c.isActive()) {
-                    cname.append(c.getName());
-                    for(String alias : c.getAliases())
-                        cname.append("/"+alias);
-                    // name/alias1/alias2
-                    em.addField(cname.toString(), c.getDescription() + "\n" + c.getArgsN(), false);
-                    cname.setLength(0);
+            if(args.length==0) {
+                StringBuilder cname = new StringBuilder();
+                for (CommandsHandler.BotCommand c : commands) {
+                    if (c.isVisible() && c.isActive()) {
+                        cname.append(c.getName());
+                        for (String alias : c.getAliases())
+                            cname.append("/" + alias);
+                        // name/alias1/alias2
+                        em.addField(cname.toString(), c.getDescription() + "\n" + c.getArgsN(), false);
+                        cname.setLength(0);
+                    }
                 }
+            } else {
+                CommandsHandler.BotCommand c = commands.find(m->{
+                    return m.name.equals(args[0]);
+                });
+                if(c==null)
+                    c=commands.find(m-> m.getAliases().find(a->a.equals(args[0])) != null);
+                if (c==null) {
+                    sendReply(e.getMessage(), "Command not found.");
+                    return;
+                }
+                StringBuilder cname = new StringBuilder();
+                cname.append(c.name);
+                for(String n : c.getAliases()) {
+                    cname.append("/"+n);
+                }
+                em.addField(c.getAliases().isEmpty() ? "Name" : "Aliases", cname.toString(), false);
+                em.addField("Description", c.getDescription(), false);
+                cname.setLength(0);
             }
-            em.description("Подсказка: команды имеют алиасы, например, команду help можно вызвать написав "+prefix+"help или "+prefix+"хелп");
+            em.author("Подсказка: команды имеют алиасы, например, команду help можно вызвать написав "+prefix+"help или "+prefix+"хелп", "", "");
             SendUtils.sendEmbedReply(em.build(), e.getMessage());
         }).setAliases(Seq.with("хелп"));
         Fun.load();
