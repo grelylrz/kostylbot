de37f00e2db8d82250249bb9ed7e2cb7a25503d8
grely <grely@grely.grely>
2025-05-17 21:00:40 +0500

    nsfw channel check

--- src/main/java/icu/grely/bot/commands/NSFW.java
+++ src/main/java/icu/grely/bot/commands/NSFW.java
@@ -1,6 +1,8 @@
 package icu.grely.bot.commands;
 
 import arc.util.Log;
+import discord4j.core.object.entity.channel.GuildMessageChannel;
+import discord4j.core.object.entity.channel.TextChannel;
 import icu.grely.nsfw.R34;
 
 import java.util.List;
@@ -12,8 +14,17 @@ public class NSFW {
     public static void load() {
         registerCommand("r34", "Ох зря я сюда полез...", "<tags>", (e, args)->{
             try {
-                List<String> l = R34.fetchGelbooru(String.join(" ", args), 10);
-                sendReply(e.getMessage(), "Результатов: "+l.size());
+                List<String> pr = R34.fetchGelbooru(String.join(" ", args), 10);
+                e.getMessage().getChannel().ofType(TextChannel.class).subscribe(ch->{
+                    if(ch.isNsfw()) {
+                        StringBuilder sb = new StringBuilder();
+                        for(String n : pr)
+                            sb.append(n+"\n");
+                        sendReply(e.getMessage(), sb.toString());
+                    } else {
+                        sendReply(e.getMessage(), "Результатов: "+pr.size()+"\nНе NSFW канал!");
+                    }
+                });
             } catch (Exception ex) {
                 sendReply(e.getMessage(), "Что то не так!");
                 Log.err(ex);
