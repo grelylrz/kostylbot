f85d14b0b91c40735850fc9004d3ca04de3c046c
grely <grely@grely.grely>
2025-05-17 20:38:12 +0500

    fix r34

--- src/main/java/icu/grely/nsfw/R34.kt
+++ src/main/java/icu/grely/nsfw/R34.kt
@@ -22,6 +22,7 @@ class R34 {
                 val request = Request.Builder()
                     .url(url)
                     .get()
+                    .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
                     .build()
 
                 Okclient.newCall(request).execute().use { response ->
