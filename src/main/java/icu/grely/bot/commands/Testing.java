a92c32988bd96c05e5a3bae8dc62d3a1f815c9f0
grely <grely@grely.grely>
2025-05-21 22:59:07 +0500

    uh

--- src/main/java/icu/grely/bot/commands/Testing.java
+++ src/main/java/icu/grely/bot/commands/Testing.java
@@ -4,6 +4,7 @@ import discord4j.discordjson.possible.Possible;
 
 import static icu.grely.bot.SendUtils.sendReply;
 import static icu.grely.bot.commands.CommandsHandler.registerCommand;
+import static icu.grely.bot.commands.CommandsHandler.setCategory;
 
 public class Testing {
     public static void load() {
